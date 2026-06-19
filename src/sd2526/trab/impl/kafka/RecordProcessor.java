6bffbef7b9c83dfd9cb3df1c3ee93fa34e7856d8
Lourenço Beato <lourenco.silva.beato@gmail.com>
2026-06-19 17:23:27 +0100

    Clean main: README only

--- src/sd2526/trab/impl/kafka/RecordProcessor.java
+++
@@ -1,9 +0,0 @@
-package sd2526.trab.impl.kafka;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface RecordProcessor {
-	
-	void onReceive(ConsumerRecord<String, String> r);
-
-}
